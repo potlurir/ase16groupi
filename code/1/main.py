9c41da44414c2c6fc599cdcfc5648009781f32c5
rpotluri12 <raghav.potluri12@gmail.com>
2016-08-21 15:44:57 -0400

    Initial Commit, All required files with initial text added

---
+++ code/1/main.py
@@ -0,0 +1,4 @@
+import utest, who1
+
+if __name__ == "__main__":
+    print "Main Program Executing"
---
+++ code/1/who1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# Test Program by Raghav Potluri
+# Group i
+
+print "Hey! This is a test program."
