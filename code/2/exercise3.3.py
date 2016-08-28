5676ac0cb0ff197287f0e9da2e59821848a0cc29
rpotluri12 <raghav.potluri12@gmail.com>
2016-08-28 00:08:37 -0400

    first three exercises done

---
+++ code/2/exercise3.3.py
@@ -0,0 +1,9 @@
+""" Write a function named right_justify that takes a string named s as a
+parameter and prints the string with enough leading spaces so that the last
+letter of the string is in column 70 of the display. """
+
+
+def right_justify(s):
+    print "%s%s" % (70*" ", s)
+
+right_justify("raghav")
