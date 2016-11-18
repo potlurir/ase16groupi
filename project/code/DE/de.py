dd7419a7b79a26705d5256fb464b7967629d3a88
Pritesh Ranjan <pritesh.ranjan.trivedi@gmail.com>
2016-11-17 20:58:42 -0500

    DE working great with CDOM, code cleanup required

--- project/code/DE/de.py
+++ project/code/DE/de.py
@@ -43,6 +43,7 @@ def cdom(self, obj1, obj2):
     l2 = loss(obj2, obj1)
 return abs(l1 - l2) > self.settings.cdom_delta and l1 < l2"""
 
+
 def _is_continous_dominated(C, T, limits):
     # Returns True if T dominates C.
     #pdb.set_trace()
@@ -61,7 +62,7 @@ def _is_continous_dominated(C, T, limits):
         c_norm = [normalize(val, limit[0], limit[1]) for val, limit in zip(c.objectives, limits)]
         t_norm = [normalize(val, limit[0], limit[1]) for val, limit in zip(t.objectives, limits)]
         n = len(c.objectives)
-        losses = [exp_loss(c,t,w,n) for c, t, w in zip(c_norm, t_norm, [-1, 1])]
+        losses = [exp_loss(c,t,w,n) for c, t, w in zip(c_norm, t_norm, [1, 1])]
         return sum(losses)/float(n)
 
     l1 = loss(C, T)  # l1 is loss associated with picking C over T
@@ -69,9 +70,6 @@ def _is_continous_dominated(C, T, limits):
     return l2 < l1  # Return true if the loss associated with picking T is less
     # cst = (T.objectives.cost - C.objectives.cost)/float(cost_normalizer)
     # sat = (T.objectives.satisfaction - C.objectives.satisfaction) / float(sat_normalizer)
-
-
-
 
 
 def _is_binary_dominated(C, T):
@@ -120,23 +118,23 @@ def differential_evolution(model_=NRP, population_size=40, f=0.3):
     limits = [[cost_max, cost_min], [sat_max, sat_min]]
     # reduce population size. Reject bad candidates
     # Pick two candidates at random, and kill the one that is dominated.
-    # for _ in range(population_size * 2):
-    #
-    #     xyz = len(population)-1
-    #     #print(xyz)
-    #     a, b = [random.randint(0, xyz) for _ in range(2)]
-    #     #print(a,b)
-    #     if _is_binary_dominated(population[a], population[b]):
-    #         population.pop(a)
-    #     elif _is_binary_dominated(population[b], population[a]):
-    #         #pdb.set_trace()
-    #         population.pop(b)
+    for _ in range(population_size * 2):
+
+        xyz = len(population)-1
+        #print(xyz)
+        a, b = [random.randint(0, xyz) for _ in range(2)]
+        #print(a,b)
+        if _is_binary_dominated(population[a], population[b]):
+            population.pop(a)
+        elif _is_binary_dominated(population[b], population[a]):
+            #pdb.set_trace()
+            population.pop(b)
 
     print ('Initial population size = {0}'.format(len(population)))
     #plot_graph(population, None)
     #initial_population = deepcopy(population)
     new_candidates = []
-    for _ in range(500):
+    for _ in range(2000):
         while 1:
             a,b,c = [random.randint(0, len(population)-1) for _ in range(3)]
             #print('a = {0} b = {1} c = {2}'.format(a, b, c))
@@ -159,18 +157,30 @@ def differential_evolution(model_=NRP, population_size=40, f=0.3):
         objectives = model.evaluate(T)
 
         if objectives is not None:
-            if _is_continous_dominated(C, T, limits):  # If C is dominated by T
-            # if _is_binary_dominated(C, T): # If C is dominated by T
-                print("T.objectives = {0}".format(T.objectives))
-                print("C.objectives = {0}".format(C.objectives))
+            # if _is_continous_dominated(C, T, limits):  # If C is dominated by T
+            if _is_binary_dominated(C, T): # If C is dominated by T
+                #print("T.objectives = {0}".format(T.objectives))
+                #print("C.objectives = {0}".format(C.objectives))
                 new_candidates.append(deepcopy(T))
 
-                print (" C was replaced by T")
+                #print (" C was replaced by T")
                 population[c] = T
         else:
             print("The mutant was not acceptable")
+    #improved_population = deepcopy(population)
+    pdb.set_trace()
+    for can in new_candidates:
+        for can1 in new_candidates:
+            if _is_binary_dominated(can1, can):
+                new_candidates.remove(can1)
 
-    return new_candidates, population
+    # for i in range(len(population)-1):
+    #     for j in range(len(population)-1):
+    #         if _is_binary_dominated(population[i], population[j]):
+    #             improved_population.pop(i)
+    #             continue
+
+    return new_candidates, None
 
 
 def plot_graph(ini_pop, population, marker='.'):
