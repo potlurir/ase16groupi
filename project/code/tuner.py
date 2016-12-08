44df6b4cfded6152a9e385ba7998fd71ca3b0aa4
Pritesh Ranjan <pritesh.ranjan.trivedi@gmail.com>
2016-12-07 22:51:46 -0500

    Final commit in Hyper-parameter tuning module

--- project/code/tuner.py
+++ project/code/tuner.py
@@ -9,21 +9,6 @@ import pdb
 
 def generate_random_f(): return round(random.random(), 2)
 
-"""
-Now the question was for a maximization problem as highlighted in Q-2a.
-So if it has to come in this function e_new has to be less than e_old.
-Since the function has to return probability the valid return value has to be between 0 and 1.
-We compute probability by raising e to an exponent. Since e > 1 (2.73),
-the exponent has to be negative to make sure that probability is less than 1.
-So the numerator of the exponent has to be (e_new - e_old).
-
-Most have you have taken t=k/k_max . At the start t = k/k_max will be a small value,
-so if you use P = e^(e_new - e_old)/t, P will be a small value considering the numerator
-is negative and t is close to 0. But ideally you need the probability to be large at the start.
-Hence, you will need to subtract the denominator with 1.
-Thus, P = e^(e_new - e_old)/(1-t) where t =k/k_max
-"""
-
 
 def probability(old, new, k): return math.exp((new - old) / k)
 # I am using it as random.random() < probability(), hence I need probability to be small at start.
@@ -49,19 +34,21 @@ def energy(val, _max, _min):
 
 def simulated_annealing_for_de(de):
 
-    n = 30
+    n = 5
     m = 30
     K_MAX = 1000
-    #_max, _min = normalize(de)  # This is going to be costly.
+    # _max, _min = normalize(de)  # This is going to be costly.
+    # Hence, do this
     _min = 0
-    _max = de.population_size
+    _max = de.population_size  # This would be awesome, hence perfect candidate for max value
     best_f = generate_random_f()
     best_ube = de.evaluate(f=best_f)
     current_energy = best_energy = energy(best_ube, _max, _min)
     k = 1
+    print("n*m,  Number of better candidates generated, Best f ")
     for i in xrange(n):
         print()
-        print(', {0}, best_ube = {2} , best_f = {3} :{1:.2f},\t'.format(m * i, best_energy, best_ube, best_f), end="")
+        print('{0} {1} {2},\t'.format(m * i, best_ube, best_f), end="")
         for j in range(m):
             next_f = generate_random_f()
             next_ube = de.evaluate(f=next_f)
@@ -86,7 +73,7 @@ def simulated_annealing_for_de(de):
 
 
 if __name__ == "__main__":
-    de = DifferentialEvolution(population_size=300)
+    de = DifferentialEvolution(population_size=100)
     de.generate_population()
     asdf = simulated_annealing_for_de(de)
     #pdb.set_trace()
